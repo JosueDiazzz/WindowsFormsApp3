443128767f696dc4eef98b7bcf5c78b70b1e7cd0
Alumnos <Alumnos@LABC2PC11>
2022-09-30 12:20:42 -0600

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp3/BLL/SedesBLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.BLL
+{
+    class SedesBLL
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set;}
+        public string Ubicacion { get; set; }
+
+
+        }
---
+++ WindowsFormsApp3/DAL/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp3.Properties;
+
+namespace WindowsFormsApp3.DAL
+{
+    public class Database
+    {
+
+
+        public static string getStrconnection()
+        {
+            return Settings.Default.empleadosConnectionString;
+                }
+        public SqlConnection GetConnection()
+        { 
+         SqlConnection Con = new SqlConnection(getStrconnection());
+            return Con;
+}
